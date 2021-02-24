4cc6ac6903c159d505bb14d89c4f5a2fd4c6d380
Sebastian <denzilbao@gmail.com>
2021-02-24 17:52:45 +0800

    save progress.

--- app/components/authentication/router.js
+++ app/components/authentication/router.js
@@ -1,13 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const authenticationController = require("./controller");
-const passport = require("passport");
-
-// router.get(
-//   "/",
-//   passport.authenticate("jwt", { session: false }),
-//   accountController.authentication
-// );
 
 router.post("/register", authenticationController.register);
 
