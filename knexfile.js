083c005fb4a0f22024855ed43bd5d0cabdf3d5f5
Sebastian <denzilbao@gmail.com>
2021-02-24 18:22:14 +0800

    setup database for heroku.

--- knexfile.js
+++ knexfile.js
@@ -18,7 +18,13 @@ const options = {
   },
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      ssl: true,
+    },
     migrations: {
       directory: __dirname + "/database/migrations",
     },
