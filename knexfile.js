d5cf35215e363d62079d6afbc76cb0b2901f4501
Sebastian <denzilbao@gmail.com>
2021-02-24 18:24:27 +0800

    setup database for heroku.

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,7 @@ const options = {
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
-      ssl: true,
+      ssl: { rejectUnauthorized: false },
     },
     migrations: {
       directory: __dirname + "/database/migrations",
