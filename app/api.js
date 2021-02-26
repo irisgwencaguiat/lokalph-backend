562afac9e91148b6fa89e4eb34e577ee2eaf99f3
Sebastian <denzilbao@gmail.com>
2021-02-26 16:02:06 +0800

    Custom user role route guard.

--- app/api.js
+++ app/api.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const api = express();
 const authenticationRouter = require("./components/authentication/router");
-const passport = require("passport");
 const middleware = require("./middleware");
 
 api.use("/authentication", authenticationRouter);
@@ -9,11 +8,11 @@ api.use("/authentication", authenticationRouter);
 api.post(
   "/test",
   [
-    passport.authenticate("jwt", { session: false }),
-    middleware.authentication.grantAccess(["admin"]),
+    middleware.authentication.passportAuthenticate,
+    middleware.authentication.grantAccess(["customer", "seller", "admin"]),
   ],
   (request, response) => {
-    console.log(request.body);
+    response.status(200).json(request.body);
   }
 );
 
--- app/middleware.js
+++ app/middleware.js
@@ -1,7 +1,31 @@
 const httpResource = require("./http_resource");
+const passport = require("passport");
 
 const middleware = {
   authentication: {
+    passportAuthenticate: (request, response, next) => {
+      passport.authenticate("jwt", { session: false }, (error, user) => {
+        if (error)
+          return response.status(401).json(
+            httpResource({
+              success: false,
+              code: 401,
+              message: "You are not authorized to access this route.",
+            })
+          );
+        if (!user)
+          return response.status(401).json(
+            httpResource({
+              success: false,
+              code: 401,
+              message: "You are not authorized to access this route.",
+            })
+          );
+        request.user = user;
+        next();
+      })(request, response, next);
+    },
+
     grantAccess: (roles) => {
       return async (request, response, next) => {
         const { type } = request.user.account_type;
