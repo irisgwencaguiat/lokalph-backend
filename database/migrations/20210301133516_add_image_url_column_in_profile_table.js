afa72d3703abf9a89a2eb66786d854af83c30feb
Iris Gwen J. Caguiat <redseijuro@gmail.com>
2021-03-01 14:13:36 +0800

    backend/46 Change profile default image URL.

--- database/migrations/20210301133516_add_image_url_column_in_profile_table.js
+++ database/migrations/20210301133516_add_image_url_column_in_profile_table.js
@@ -2,7 +2,9 @@ exports.up = function (knex) {
   return knex.schema.table("profile", function (table) {
     table
       .string("image_url")
-      .defaultTo("https://dogelore.fandom.com/wiki/Doge?file=Doge.jpg");
+      .defaultTo(
+        "https://i.pinimg.com/originals/8d/ec/f9/8decf9caed777b8d0d698e01270ce308.png"
+      );
   });
 };
 
