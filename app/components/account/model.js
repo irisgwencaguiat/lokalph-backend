099873dc2ed643bcb129273dc5147777b80d2117
Sebastian <denzilbao@gmail.com>
2021-03-04 15:56:24 +0800

    save progress.

--- app/components/account/model.js
+++ app/components/account/model.js
@@ -18,30 +18,44 @@ const accountModel = {
       });
   },
   getAccountDetails: async (id) => {
-    return await knex(`${accountModel.tableName} as account`)
-      .where("account.id", id)
-      .then(async (result) => {
-        const account = result[0];
-        const profile = await knex("profile")
-          .where("profile.id", account.profile_id)
-          .then((result2) => result2[0]);
-        const account_type = await knex("account_type")
-          .where("account_type.id", account.account_type_id)
-          .then((result2) => result2[0]);
-        account.profile = Object.assign({}, profile);
-        account.account_type = Object.assign({}, account_type);
-        delete account.password;
-        return account;
-      });
+    return (
+      (await knex(`${accountModel.tableName} as account`)
+        .where("account.id", id)
+        .then(async (result) => {
+          if (!result[0]) return null;
+          const account = result[0];
+          const profile = await knex("profile")
+            .where("profile.id", account.profile_id)
+            .then((result2) => result2[0]);
+          const account_type = await knex("account_type")
+            .where("account_type.id", account.account_type_id)
+            .then((result2) => result2[0]);
+          account.profile = Object.assign({}, profile);
+          account.account_type = Object.assign({}, account_type);
+          delete account.password;
+          return account;
+        })) || null
+    );
   },
   getAccountDetailsByEmail: async (email) => {
-    return await knex
-      .select("account.id", "account.password")
-      .from(accountModel.tableName)
-      .where("email", email)
-      .then((result) => {
-        return result[0];
-      });
+    return (
+      (await knex(`${accountModel.tableName} as account`)
+        .where("account.email", email)
+        .then(async (result) => {
+          if (!result[0]) return null;
+          const account = result[0];
+          const profile = await knex("profile")
+            .where("profile.id", account.profile_id)
+            .then((result2) => result2[0]);
+          const account_type = await knex("account_type")
+            .where("account_type.id", account.account_type_id)
+            .then((result2) => result2[0]);
+          account.profile = Object.assign({}, profile);
+          account.account_type = Object.assign({}, account_type);
+          delete account.password;
+          return account;
+        })) || null
+    );
   },
   updateAccountType: async (id) => {
     return knex(accountModel.tableName).where("id", id).update({
