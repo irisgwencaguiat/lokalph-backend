8fe14c3a4d549b71fa95c83276f11beaef8b606e
Iris Gwen J. Caguiat <redseijuro@gmail.com>
2021-03-22 16:04:21 +0800

    backend/79 get products details by slug

--- app/components/product/model.js
+++ app/components/product/model.js
@@ -1,6 +1,4 @@
 const knex = require("../../../database/knex");
-const shopModel = require("../shop/model");
-const imageModel = require("../image/model");
 const shippingMethodModel = require("../shipping-method/model");
 const keywordModel = require("../keyword/model");
 
@@ -46,46 +44,122 @@ const productModel = {
       .returning(["id"])
       .then((result) => result[0]);
   },
+  async getProductAccountDetails(id) {
+    return (
+      (await knex(`account`)
+        .where("id", id)
+        .then(async (result) => {
+          if (!result[0]) return null;
+          const account = result[0];
+          const profile = await knex("profile")
+            .where("profile.id", account.profile_id)
+            .then((result2) => result2[0]);
+          const account_type = await knex("account_type")
+            .where("account_type.id", account.account_type_id)
+            .then((result2) => result2[0]);
+          const stripe =
+            (await knex("stripe")
+              .where("stripe.id", account.stripe_id)
+              .then((result2) => result2[0])) || null;
+          account.profile = Object.assign({}, profile);
+          account.account_type = Object.assign({}, account_type);
+          account.stripe = stripe ? Object.assign({}, stripe) : null;
+          delete account.password;
+          return account;
+        })) || null
+    );
+  },
+  async getProductShopDetails(id) {
+    return await knex("shop")
+      .where("id", id)
+      .then(async (result) => {
+        const shop = result[0];
+        const account = await productModel.getProductAccountDetails(
+          shop.account_id
+        );
+        const address = await knex("address")
+          .where("id", shop.address_id)
+          .then(async (result) => {
+            return result[0];
+          });
+        shop.account = Object.assign({}, account);
+        shop.address = Object.assign({}, address);
+        delete shop.account_id;
+        delete shop.address_id;
+        delete shop.account.stripe_id;
+        delete shop.account.profile_id;
+        delete shop.account.account_type_id;
+        delete shop.account.address_id;
+        delete shop.account.stripe;
+        return shop;
+      });
+  },
+  async getProductCategoryDetails(id) {
+    return await knex("product_category")
+      .where("id", id)
+      .then((result) => result[0]);
+  },
+  async getProductConditionDetails(id) {
+    return await knex("product_condition")
+      .where("id", id)
+      .then((result) => result[0]);
+  },
+  async getProductImagesDetails(id) {
+    const productImagesDetails = await knex("product_image")
+      .where("product_id", id)
+      .then((result) => result);
+    return await Promise.all(
+      productImagesDetails.map(async (image) => {
+        return await knex("image")
+          .where("id", image.image_id)
+          .then((result) => result[0]);
+      })
+    );
+  },
+  async getProductKeywordsDetails(id) {
+    const productKeywordsDetails = await knex("product_keyword")
+      .where("product_id", id)
+      .then((result) => result);
+    return await Promise.all(
+      productKeywordsDetails.map(async (keyword) => {
+        return await keywordModel.getKeywordDetails(keyword.keyword_id);
+      })
+    );
+  },
+  async getProductShippingMethodsDetails(id) {
+    const productShippingMethodsDetails = await knex("product_shipping_method")
+      .where("product_id", id)
+      .then((result) => result);
+    return await Promise.all(
+      productShippingMethodsDetails.map(async (method) => {
+        return await shippingMethodModel.getShippingMethodDetails(
+          method.shipping_method_id
+        );
+      })
+    );
+  },
   async getProductDetails(id) {
     return await knex(productModel.tableName)
       .where("id", id)
       .then(async (result) => {
         const product = result[0];
-        const productCategory = await knex("product_category")
-          .where("id", product.product_category_id)
-          .then((result2) => result2[0]);
-        const productCondition = await knex("product_condition")
-          .where("id", product.product_condition_id)
-          .then((result2) => result2[0]);
-        const productImagesDetails = await knex("product_image")
-          .where("product_id", product.id)
-          .then((result2) => result2);
-        const images = await Promise.all(
-          productImagesDetails.map(async (image) => {
-            return await imageModel.getImageDetails(image.image_id);
-          })
+        const productCategory = await productModel.getProductCategoryDetails(
+          product.product_category_id
         );
-        const productKeywordsDetails = await knex("product_keyword")
-          .where("product_id", product.id)
-          .then((result2) => result2);
-        const keywords = await Promise.all(
-          productKeywordsDetails.map(async (keyword) => {
-            return await keywordModel.getKeywordDetails(keyword.keyword_id);
-          })
+        const productCondition = await productModel.getProductConditionDetails(
+          product.product_condition_id
         );
-        const productShippingMethodsDetails = await knex(
-          "product_shipping_method"
-        )
-          .where("product_id", product.id)
-          .then((result2) => result2);
-        const shippingMethods = await Promise.all(
-          productShippingMethodsDetails.map(async (method) => {
-            return await shippingMethodModel.getShippingMethodDetails(
-              method.shipping_method_id
-            );
-          })
+
+        const images = await productModel.getProductImagesDetails(product.id);
+
+        const keywords = await productModel.getProductKeywordsDetails(
+          product.id
         );
-        const shop = await shopModel.getShopDetails(product.shop_id);
+
+        const shippingMethods = await productModel.getProductShippingMethodsDetails(
+          product.id
+        );
+        const shop = await productModel.getProductShopDetails(product.shop_id);
         product.shop = Object.assign({}, shop);
         product.category = Object.assign({}, productCategory);
         product.condition = Object.assign({}, productCondition);
@@ -153,6 +227,40 @@ const productModel = {
       .count("id")
       .where("shop_id", shopId)
       .then((result) => parseInt(result[0].count) || null);
+  },
+  async getProductDetailsBySlug(slug) {
+    return await knex("product")
+      .where("slug", slug)
+      .then(async (result) => {
+        const product = result[0];
+        const productCategory = await productModel.getProductCategoryDetails(
+          product.product_category_id
+        );
+        const productCondition = await productModel.getProductConditionDetails(
+          product.product_condition_id
+        );
+
+        const images = await productModel.getProductImagesDetails(product.id);
+
+        const keywords = await productModel.getProductKeywordsDetails(
+          product.id
+        );
+
+        const shippingMethods = await productModel.getProductShippingMethodsDetails(
+          product.id
+        );
+        const shop = await productModel.getProductShopDetails(product.shop_id);
+        product.shop = Object.assign({}, shop);
+        product.category = Object.assign({}, productCategory);
+        product.condition = Object.assign({}, productCondition);
+        product.images = Object.assign([], images);
+        product.shipping_methods = Object.assign([], shippingMethods);
+        product.keywords = Object.assign([], keywords);
+        delete product.shop_id;
+        delete product.product_category_id;
+        delete product.product_condition_id;
+        return product;
+      });
   },
 };
 
