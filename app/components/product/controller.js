bac5dde534cb3c18a57675cda850709510f53984
Iris Gwen J. Caguiat <redseijuro@gmail.com>
2021-03-29 13:31:44 +0800

    backend/96 Create Product Offer API

--- app/components/product/controller.js
+++ app/components/product/controller.js
@@ -547,6 +547,45 @@ const productController = {
       );
     }
   },
+  async createProductOffer(request, response) {
+    try {
+      const { id } = request.user;
+      const { note, quantity, total_price, product_id, shop_id } = request.body;
+
+      if (!quantity) throw "Quantity can't be empty.";
+      if (!total_price) throw "Total price can't be empty.";
+      if (quantity < 1) throw "Quantity can't be less than 1.";
+      if (total_price < 1) throw "Total price can't be less than 1.";
+      if (note.length > 250) throw "Note shouldn't exceed 250 characters.";
+      const createdProductOffer = await productModel.createProductOffer({
+        note,
+        quantity,
+        total_price,
+        product_id,
+        shop_id,
+        account_id: id,
+      });
+      const productOffer = await productModel.getProductOfferDetailsById(
+        createdProductOffer.id
+      );
+      response.status(200).json(
+        httpResource({
+          success: true,
+          code: 200,
+          message: "Successfully got records.",
+          data: productOffer,
+        })
+      );
+    } catch (error) {
+      response.status(400).json(
+        httpResource({
+          success: false,
+          code: 400,
+          message: error,
+        })
+      );
+    }
+  },
 };
 
 module.exports = productController;
---
+++ database/migrations/20210329113125_create_product_offer_table.js
@@ -0,0 +1,27 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("product_offer", function (table) {
+    table.increments();
+    table.string("note");
+    table.string("status").defaultTo("pending");
+    table.integer("quantity");
+    table.decimal("total_price", 14, 2);
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+    table
+      .integer("cancelled_by")
+      .references("id")
+      .inTable("account")
+      .defaultTo(null);
+    table.integer("shop_id").references("id").inTable("shop");
+    table.integer("product_id").references("id").inTable("product");
+    table.integer("account_id").references("id").inTable("account");
+    table
+      .integer("shopping_method_id")
+      .references("id")
+      .inTable("shipping_method");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("product_offer");
+};
